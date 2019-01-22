dc88bd1dd208ba8639cdc8350069ab6455a6e33e
Manfred Graf <manfred.graf91@gmail.com>
2019-01-22 22:04:26 +0100

    install unclutter (hides idle curser)

--- ModbrosMonitoring/install.sh
+++ ModbrosMonitoring/install.sh
@@ -21,9 +21,10 @@ echo "Updating your Raspberry..."
 apt-get update && apt-get upgrade -y
 
 echo "Installing necessary libraries..."
-apt-get install apache2 php7.0 libapache2-mod-php7.0 chromium-browser -y
+apt-get install apache2 php7.0 libapache2-mod-php7.0 -y
+apt-get chromium-browser -y
 apt-get install hostapd dnsmasq -y
-
+apt-get install unclutter -y
 
 # =============================
 # Stop and disable access point services
@@ -128,3 +129,4 @@ echo "done. outro bla bla"
 
 # TODO reboot ??
 
+exit 0
