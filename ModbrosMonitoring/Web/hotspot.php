b1c569f6b29c709d113b10b61a627ff8f389a8d0
Manfred Graf <manfred.graf91@gmail.com>
2019-01-22 21:55:48 +0100

    added instruction page on how to connect to the hotspot

---
+++ ModbrosMonitoring/Web/hotspot.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>ModBros Monitoring Setup</title>
+</head>
+<style>
+  body {
+    font-size: 16px;
+  }
+</style>
+<body>
+
+<div id="container">
+  <h3>ModBros Monitoring Setup</h3>
+  <p>
+    To connect this Raspberry to your wireless network please connect to the following hotspot:<br/>
+    (e.g. by using your notebook or mobile phone):
+  </p>
+  <p>
+    <b>ModBros_Configuration</b> with password <b>modbros123</b>
+  </p>
+  <p>
+    Then open a browser and navigate to:
+  </p>
+  <p>
+    <b>http://192.168.4.1/modbros</b>
+  </p>
+  <p>
+    You should now see a setup page containing instructions on how to proceed :)
+  </p>
+</div>
+
+</body>
+</html>
