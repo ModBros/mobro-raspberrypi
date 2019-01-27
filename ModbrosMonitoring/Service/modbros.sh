fa3caeb6ea5a9452fdf8c8db92dfb5f18267d9bb
Manfred Graf <manfred.graf91@gmail.com>
2019-01-27 03:28:45 +0100

    first version of modbros service

--- ModbrosMonitoring/Service/modbros.sh
+++ ModbrosMonitoring/Service/modbros.sh
@@ -2,7 +2,56 @@
 
 # TODO
 
-# check if connected to wifi
+update_pi () {
+    apt-get update
+    apt-get upgrade -y
+    apt-get autoremove --purge -y
+    apt-get autoclean -y
+}
 
-# if connected -> done
-# else -> start hotspot
+sleep 20    # startup delay
+#update_pi   # make sure everything is up to date
+
+while true; do
+
+    if ! systemctl is-active --quiet hostapd
+    then
+        # no hotspot running
+        echo "no hotspot running"
+
+        if ! [[ $(iwgetid) ]]; then
+            # not connected to wifi -> create hotspot
+            echo "not connected to wifi"
+
+            ./createaccesspoint.sh
+
+            ./stopchrome.sh
+            ./startchrome.sh http://localhost/modbros/hotspot.php
+
+             sleep 60
+
+            continue
+        else
+            # connected to wifi
+            if [[ $(ps a | grep chromium | grep -v "grep" | wc -l) -eq 0 ]]; then
+                # chrome not yet running -> check website availability
+                # TODO network discovery
+                # TODO read connection key from file to ping
+                while [[ $(curl -o /dev/null --silent --write-out '%{http_code}' localhost/modbros/notfound.php) -ne 200 ]]; do
+                    echo "website not reachable"
+                    sleep 5
+                done
+                ./startcrome.sh http://localhost
+            fi
+        fi
+    else
+    # hotstop running -> check if chrome is up
+        if [[ $(ps a | grep chromium | grep -v "grep" | wc -l) -eq 0 ]]; then
+            ./startchrome.sh http://localhost/modbros/hotspot.php
+        fi
+    fi
+
+    sleep 60
+done
+
+exit 1
