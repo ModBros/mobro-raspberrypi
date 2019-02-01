fd96c05e2aa377c174ed7ba00d540fc411ab9cee
Manfred Graf <manfred.graf91@gmail.com>
2019-02-01 20:38:23 +0100

    added page for wifi connection attempt

---
+++ ModbrosMonitoring/Web/connectwifi.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>ModBros Monitoring Setup</title>
+  <link href="./bootstrap.min.css" rel="stylesheet"/>
+</head>
+<style>
+  body {
+    font-size: 20px;
+  }
+</style>
+<body>
+
+<div id="container" class="container">
+  <div class="card mt-3">
+    <div class="card-header">
+      <h3 class="m-0">ModBros Monitoring Setup</h3>
+    </div>
+
+    <div class="card-body">
+      <p>
+          <?php
+          $ssid = shell_exec('cat /home/pi/ModbrosMonitoring/data/wifi.txt | sed -n 1p');
+          if (isset($ssid) && trim($ssid) !== '') {
+              echo 'Now trying to connect to: <b>' . $ssid . '</b>';
+          } else {
+              echo '<b>Error while trying to connect!</b>';
+          }
+          ?>
+      </p>
+    </div>
+  </div>
+</div>
+
+</body>
+</html>
