b6b928fd92927222278b41c2e69f0103e541ea83
Manfred Graf <manfred.graf91@gmail.com>
2019-02-03 22:37:46 +0100

    major improvements to service discovery + various bugfixes

--- ModbrosMonitoring/Service/modbros.sh
+++ ModbrosMonitoring/Service/modbros.sh
@@ -25,10 +25,13 @@ MOBRO_PORT='42100'
 # Global Vars
 STARTUP_DELAY=30  # in seconds
 LOOP_INTERVAL=5   # in seconds
-CHECK_INTERVAL=30 # in loops (30x5=150s -> every 2.5 minutes)
+CHECK_INTERVAL=60 # in loops (60x5=300s -> every 5 minutes)
 CURR_URL=''
 HOTSPOT_COUNTER=0
 BACKGROUND_COUNTER=0
+LAST_CHECKED_SSID=''
+LAST_CHECKED_PW=''
+MOBRO_FOUND=0
 
 # =============================
 # Functions
@@ -50,7 +53,7 @@ show_page() {
         log "show_page" "switching to page $1"
         CURR_URL="$1"
         sudo ./stopchrome.sh
-        sleep 2
+        sleep 1
         DISPLAY=:0 ./startchrome.sh "$1" &
         sleep 10
         log "show_page" "done"
@@ -70,35 +73,87 @@ create_access_point() {
 connect_wifi() {
     log "connect_wifi" "connecting to wifi: $1 $2"
     show_page ${URL_CONNECT_WIFI}
-    ./connectwifi.sh $1 $2
-    log "connect_wifi" "done"
+    sudo ./connectwifi.sh $1 $2
+    sleep 15
+    if [[ $(iwgetid) ]]; then
+        log "connect_wifi" "connected"
+        handle_connecting
+    else
+        log "connect_wifi" "not connected"
+        create_access_point
+    fi
+}
+
+try_ip() {
+    # $1 = IP, $2 = key
+    log "service_discovery" "Trying IP: $1 with key: $2"
+    if [[ $(curl -o /dev/null --silent --max-time 5 --connect-timeout 0.25 --write-out '%{http_code}' "$1:$MOBRO_PORT/discover?key=$2") -eq 200 ]]; then
+        # found MoBro application -> done
+        log "service_discovery" "MoBro application found on IP $1"
+        VERSION=$(cat "$VERSION_FILE" | sed -n 1p)  # service version number
+        PI_VERSION=$(cat /proc/device-tree/model)   # pi version (e.g. Raspberry Pi 3 Model B Plus Rev 1.3)
+        show_page "http://$1:$MOBRO_PORT?version=$VERSION&name=$PI_VERSION"
+        MOBRO_FOUND=1
+
+        # write to file to find it faster on next boot
+        echo "$1" > "${HOSTS_FILE}"
+    fi
 }
 
 service_discovery() {
     # search available IPs on the network
     log "service_discovery" "starting service discovery"
 
-    FOUND=0
+    MOBRO_FOUND=0
     sudo arp-scan --interface=wlan0 --localnet | grep -E -o "([0-9]{1,3}[\.]){3}[0-9]{1,3}" >> "${HOSTS_FILE}"
     KEY=$(cat "$WIFI_FILE" | sed -n 3p)         # 3rd line contains MoBro connection key
-    VERSION=$(cat "$VERSION_FILE" | sed -n 1p)  # service version number
-    PI_VERSION=$(cat /proc/device-tree/model)   # pi version (e.g. Raspberry Pi 3 Model B Plus Rev 1.3)
-    while read IP; do
-        log "service_discovery" "Trying IP: $IP with key: $KEY"
-        if [[ $(curl -o /dev/null --silent --write-out '%{http_code}' "$IP:$MOBRO_PORT/discover?key=$KEY") -eq 200 ]]; then
-            # found MoBro application -> done
-            log "service_discovery" "MoBro application found"
-            show_page "$IP:$MOBRO_PORT?version=$VERSION&name=$PI_VERSION"
-            FOUND=1
 
-            # write to file to find it faster on next boot
-            echo "$IP" > "${HOSTS_FILE}"
+    while read IP; do
+        try_ip "$IP" "$KEY"
+        if [[ ${MOBRO_FOUND} -eq 1 ]]; then
+            # found MoBro application -> done
             break
         fi
-        sleep 1
     done < "${HOSTS_FILE}"
 
-    if [[ ${FOUND} -ne 1 ]]; then
+    # get the 3rd digit of our own IP on the wireless network for effective scanning
+    # e.g. 192.168.0.180 => 0
+    PI_IP_3=$(ifconfig wlan0 | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p' | cut -d . -f 3)
+
+    if ! [[ -z ${PI_IP_3} ]]; then
+        log "service_discovery" "got 3rd PI IP digit. trying all IPs in range 192.168.$PI_IP_3.X"
+        for i in {0..255}
+        do
+            try_ip "192.168.$PI_IP_3.$i" "$KEY"
+            if [[ ${MOBRO_FOUND} -eq 1 ]]; then
+                # found MoBro application -> done
+                break
+            fi
+        done
+    fi
+
+
+    if [[ ${MOBRO_FOUND} -ne 1 ]]; then
+        # couldn't find application -> try all IPs in range 192.168.X.X
+        log "service_discovery" "using 'brute force' and just trying ips in range 192.168.X.X"
+        for i in {0..255}
+        do
+            for j in {0..255}
+            do
+                try_ip "192.168.$i.$j" "$KEY"
+                if [[ ${MOBRO_FOUND} -eq 1 ]]; then
+                    # found MoBro application -> done
+                    break
+                fi
+            done
+            if [[ ${MOBRO_FOUND} -eq 1 ]]; then
+                # found MoBro application -> done
+                break
+            fi
+        done
+    fi
+
+    if [[ ${MOBRO_FOUND} -ne 1 ]]; then
         # couldn't find application -> delete IPs
         log "service_discovery" "no MoBro application found"
         truncate -s 0 "${HOSTS_FILE}"
@@ -113,24 +168,29 @@ handle_connecting() {
 }
 
 hotspot_check() {
+    SSID=$(cat "$WIFI_FILE" | sed -n 1p)      # 1st line contains SSID
+    PW=$(cat "$WIFI_FILE" | sed -n 2p)        # 2nd line contains PW
+    if ! [[ -z ${SSID} || -z ${PW} ]]; then
+        if [[ ${SSID} != ${LAST_CHECKED_SSID} ]]; then
+            if [[ ${PW} != ${LAST_CHECKED_PW} ]]; then
+                # if there is new access data -> instantly try connecting
+                LAST_CHECKED_SSID=${SSID}
+                LAST_CHECKED_PW=${PW}
+                log "hotspot_check" "trying to connect with $SSID and $PW"
+                connect_wifi "$SSID" "$PW"
+                return
+            fi
+        fi
+    fi
+
     HOTSPOT_COUNTER=$((HOTSPOT_COUNTER+1))
-    if [[ ${HOTSPOT_COUNTER} -ge 20 ]]; then
+    if [[ ${HOTSPOT_COUNTER} -ge ${CHECK_INTERVAL} ]]; then
         log "hotspot_check" "start hotspot check"
         HOTSPOT_COUNTER=0
-        SSID=$(cat "$WIFI_FILE" | sed -n 1p)      # 1st line contains SSID
-        PW=$(cat "$WIFI_FILE" | sed -n 2p)        # 2nd line contains PW
         if ! [[ -z ${SSID} || -z ${PW} ]]; then
             # wifi configured -> try again
             log "hotspot_check" "trying to connect with $SSID and $PW"
             connect_wifi "$SSID" "$PW"
-            sleep 15
-            if [[ $(iwgetid) ]]; then
-                log "hotspot_check" "connected"
-                handle_connecting
-            else
-                log "hotspot_check" "not connected"
-                create_access_point
-            fi
         fi
     else
         log "hotspot_check" "skipping hotspot check"
@@ -139,7 +199,7 @@ hotspot_check() {
 
 background_check() {
     BACKGROUND_COUNTER=$((BACKGROUND_COUNTER+1))
-    if [[ ${BACKGROUND_COUNTER} -ge 20 ]]; then
+    if [[ ${BACKGROUND_COUNTER} -ge ${CHECK_INTERVAL} ]]; then
         log "background_check" "starting background check"
         BACKGROUND_COUNTER=0
         service_discovery
@@ -184,7 +244,6 @@ while true; do
         fi
     else
         log "Main" "hotspot up"
-
 
         # hotstop running -> check if chrome is up
         if [[ $(ps ax | grep chromium | grep -v "grep" | wc -l) -eq 0 ]]; then
