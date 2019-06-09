a9991394cb1bcb6324d9fa5ec071a00f95e738e4
Manfred Graf <manfred.graf91@gmail.com>
2019-06-10 00:43:10 +0200

    added log page

---
+++ ModbrosMonitoring/web/log/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Log</title>
+</head>
+<body>
+<div id="container">
+    <?php
+    $base = '/home/modbros/ModbrosMonitoring/log/log';
+    $log = nl2br(file_get_contents($base . '.txt'));
+    if (isset($_GET['all'])) {
+        for ($i = 0; $i < 10; $i++) {
+            $path = $base . '_' . $i . '.txt';
+            if (file_exists($path)) {
+                $log .= "<br/><br/>";
+                $log .= "===========================================================================================<br />";
+                $log .= "========================================= LOG " . $i . " ============================================<br />";
+                $log .= "===========================================================================================<br />";
+                $log .= nl2br(file_get_contents($path));
+            }
+        }
+    }
+    echo $log;
+    ?>
+</div>
+</body>
+</html>
