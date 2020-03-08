9242b1df25dc5b92510068ba85fb03222aea928f
Manfred Graf <manfred.graf91@gmail.com>
2020-03-08 19:44:02 +0100

    updated redirecting index file with footer

---
+++ ModbrosMonitoring/web/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="refresh" content="0; url=/modbros/"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1"/>
+  <title>ModBros Monitoring Setup</title>
+  <link rel="shortcut icon" href="./resources/favicon.ico" type="image/x-icon"/>
+  <link href="bootstrap.min.css" rel="stylesheet"/>
+</head>
+
+<?php
+include 'constants.php';
+?>
+
+<style>
+  body {
+    font-size: 22px;
+  }
+</style>
+
+<body>
+<div id="container" class="container">
+  <p class="mt-3 alert alert-info font-weight-bold">If not automatically redirected please <a href="./modbros/index.php">click here</a>.</p>
+  <?php include './includes/footer.php' ?>
+</div>
+</body>
+</html>
