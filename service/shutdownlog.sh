3b4c8392ab207139243dcd084a8cc8052b6adb26
Manfred Graf <manfred.graf91@gmail.com>
2021-03-29 23:48:27 +0200

    feat(log): preserve log files on shutdown

---
+++ service/shutdownlog.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+LOG_DIR='/home/modbros/mobro-raspberrypi/log'
+LOG_FILE='/tmp/mobro_log'
+
+touch "$LOG_DIR/test123"
+
+# first remount the /home partition writable
+mount -o remount,rw /home
+
+# copy log files to preserve the previous 10 starts
+if [[ -f "$LOG_FILE" ]]; then
+  for i in {8..0}; do
+    if [[ -f "$LOG_DIR/log_$i.txt" ]]; then
+      mv -f "$LOG_DIR/log_$i.txt" "$LOG_DIR/log_$((i + 1)).txt" 2>/dev/null
+    fi
+  done
+  cp -f "$LOG_FILE" "$LOG_DIR/log_0.txt" 2>/dev/null
+fi
